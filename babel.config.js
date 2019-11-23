4d9c05e9085e0c9f177589fe0e1feaec9c376d26
Emma Kerimkhanova <Kerimkhanova93@yandex.ru>
2019-11-23 15:09:05 +0300

    change babel

--- babel.config.js
+++ babel.config.js
@@ -10,10 +10,6 @@ const presets = [
             },
             useBuiltIns: "usage", // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали выше.
             corejs: "3.0.0", // явно проставить версию corejs
-            "targets": { // указать цели, для полифилов
-                "esmodules": true, // es модули 
-                "ie": "11" // Internet Explorer 11
-            }
         },
     ],
 ];
